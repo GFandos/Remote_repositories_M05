ed7881085ee5654838747d4db98e48976ebde7f5
gfandos <gerard.fandos@gmail.com>
2016-11-09 18:26:25 +0100

    Starting the new project

---
+++ app/src/main/java/com/example/a47989768s/m05_remote_repos/remote_controllerFragment.java
@@ -0,0 +1,22 @@
+package com.example.a47989768s.m05_remote_repos;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class remote_controllerFragment extends Fragment {
+
+    public remote_controllerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_remote_controller, container, false);
+    }
+}
